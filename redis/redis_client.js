01a9e6975b4f4519ea9abe0907ba1080d9a20dd1
curwiot <100547410+curwiot@users.noreply.github.com>
2022-03-22 15:30:40 +0530

    redis debug

--- redis/redis_client.js
+++ redis/redis_client.js
@@ -3,28 +3,38 @@ require('dotenv').config();
 
 class redis_class {
     async set_value(key, value, mins) {
-        const url = `redis://${process.env.redisHost}:${process.env.redisPort}`;
-        var client = redis.createClient({
-            url:url
+        try {
+            const url = `redis://${process.env.redisHost}:${process.env.redisPort}`;
+            var client = redis.createClient({
+                url: url
+            }
+            );
+            client.on('error', (err) => console.log('Redis Client Error', err));
+            await client.connect()
+            await client.setEx(key, 60 * parseInt(mins), JSON.stringify(value))
+        } catch (error) {
+            console.log(error)
         }
-        );
-        client.on('error', (err) => console.log('Redis Client Error', err));
-        await client.connect()
-        await client.setEx(key, 60 * parseInt(mins), JSON.stringify(value))
-        
+
+
     }
 
     //get value
     async get_value(key) {
-        const url = `redis://${process.env.redisHost}:${process.env.redisPort}`;
-        var client = redis.createClient({
-            url:url
+        try {
+            const url = `redis://${process.env.redisHost}:${process.env.redisPort}`;
+            var client = redis.createClient({
+                url: url
+            }
+            );
+            client.on('error', (err) => console.log('Redis Client Error', err));
+            await client.connect();
+            const value = await client.get(key)
+            return JSON.parse(value)
+        } catch (error) {
+            console.log(error);
         }
-        );
-        client.on('error', (err) => console.log('Redis Client Error', err));
-        await client.connect();
-        const value = await client.get(key)
-        return JSON.parse(value)
+
     }
 }
 
